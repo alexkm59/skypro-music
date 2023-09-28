9bac86e2901843246bae5d8818c436012629f250
alexkm59 <alexkm59@yandex.ru>
2023-09-28 14:22:02 +0500

    add components of jsx

---
+++ src/components/loginArea.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+export function loginArea () {
+return(
+<div className="sidebar__personal">
+              <p className="sidebar__personal-name">Sergey.Ivanov</p>
+              <div className="sidebar__icon">
+                <svg alt="logout">
+                  <use xlinkHref="img/icon/sprite.svg#logout"></use>
+                </svg>
+              </div>
+</div>
+
+)
+
+}
---
+++ src/components/search.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+export function search () {
+return(
+    <div className="centerblock__search search">
+              <svg className="search__svg">
+                <use xlinkHref="img/icon/sprite.svg#icon-search"></use>
+              </svg>
+              <input
+                className="search__text"
+                type="search"
+                placeholder="Поиск"
+                name="search"
+              />
+            </div>
+)
+
+}
